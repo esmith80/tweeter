f8bae109aa3a9eb66a83232b91bd94889cbf2e7e
Edward Smith <edward.m.smith80@gmail.com>
2020-09-10 20:58:34 +0000

    finished error message display for no character tweets or tweets that are too long, also finished ajax for loading all tweets and displaying page with new tweeet without a page refresh

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,36 +5,11 @@
  */
 
 $( document ).ready(function() {
+
   console.log( "Document ready!" );
 
-  const tweets = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
   const renderTweets = function(tweets) {
-    // loops through tweets
-    
+    // loops through tweets    
     for (const tweet of tweets) {
       const $tweet = createTweetElement(tweet);
       $('.container').append($tweet);
@@ -82,8 +57,65 @@ $( document ).ready(function() {
     return $tweet;
   };
   
-  renderTweets(tweets);
+  const loadTweets = function () {
+    console.log('loadtweets just got called');
+    $.get('/tweets')
+      .then(function (jsonTweets) {
+        const newestFirstTweetList = jsonTweets.reverse();
+        renderTweets(newestFirstTweetList); // NOW WHAT am i supposed to return something?
+       });
+    };
 
+  // ----------- INITIAL PAGE LOAD AND EVENT LISTENERS --------
   
+    // initial loading page this will run at start of program 
+  loadTweets(); 
+
+  // event listener to submit form using JQuery using Ajax  
+  const $form = $('#post-tweet');
+  let maxCharDisplay = false;
+  let zeroCharDisplay = false;
   
-});
+  $form.submit( function (event) {
+    
+    event.preventDefault();
+    
+    // validation logic
+    let charRemaining = $('#counter').val();
+    if (charRemaining == 140) {
+      console.log('entered the charRemaining == 140 condition');
+      if (maxCharDisplay) {
+        $('#error-max-characters').slideUp();
+        maxCharDisplay = false;
+      }
+      $('#error-zero-characters').slideDown();
+      zeroCharDisplay = true;
+    } else if (charRemaining < 0) {
+      if (zeroCharDisplay) {
+        $('#error-zero-characters').slideUp();
+        zeroCharDisplay = false;
+      }
+      $('#error-max-characters').slideDown();
+      maxCharDisplay = true;
+
+    } else {
+      $('#error-zero-characters').slideUp();
+      $('#error-max-characters').slideUp();
+      zeroCharDisplay = false;
+      maxCharDisplay = false;
+      
+      // if character limit is OK, proceed with creating new tweet in database
+      // why serialize??? because server is configured to revceive form data as a query string so we need to serialize it
+      const newTweet = $(this).serialize(); //returns text=whateveryoutypein
+      $.post('/tweets', newTweet) // sending a request to server
+      .then(function (response) { // receiving the response
+        document.getElementById("post-tweet").reset();
+        $('#counter').empty().append("140");
+        $('.posted-tweet').replaceWith(loadTweets());
+        
+        });
+    }
+  });
+
+}); // end of document ready top bracket
+
